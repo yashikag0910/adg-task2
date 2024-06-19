28db885c818b7e650057944df9767e7bf1502ee0
yashikag0910 <yashikag0910@gmail>
2024-06-20 01:38:54 +0530

    update

--- adg task/ContentView.swift
+++ adg task/ContentView.swift
@@ -19,7 +19,6 @@ struct ContentView: View {
             Image("background-plain")
                 .resizable()
                 .ignoresSafeArea()
-            
             VStack{
                 Spacer()
                 Image("logo")
