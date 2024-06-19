314e53904ed4e3e7492a9188b7bfbf8ab3fd74a9
yashikag0910 <yashikag0910@gmail>
2024-06-19 22:28:39 +0530

    Initial Commit

---
+++ adg task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  adg task
+//
+//  Created by YASHIKA GUPTA on 19/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ adg task/adg_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  adg_taskApp.swift
+//  adg task
+//
+//  Created by YASHIKA GUPTA on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct adg_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
